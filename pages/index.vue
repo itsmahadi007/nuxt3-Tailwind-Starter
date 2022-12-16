807883d3cd74ca35fbd1834bf3f6fcb0c0d5998a
Mahadi Hassan <itsmahadi@gmail.com>
2022-12-16 11:33:35 +0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <NuxtLayout>
     <div class="container w-full md:max-w-3xl mx-auto pt-20">
-      <HelloWorld007 />
+      <HelloWorld007/>
     </div>
   </NuxtLayout>
 </template>
 
-<script setup>
+<script setup lang="ts">
 // import HelloWorld007 from "~/components/HelloWorld007.vue";
 
 const description = "Build an amazing Nuxt 3 app with Nuxt.Tips";
